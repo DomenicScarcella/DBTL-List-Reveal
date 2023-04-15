7e3ce8b9944309fe60bff9f9c0dfc0329299008c
unknown <ls41513@yahoo.com>
2023-04-15 15:39:33 -0400

    ListModular tests on initial rendering

--- __tests__/ListModular.test.jsx
+++ __tests__/ListModular.test.jsx
@@ -8,18 +8,28 @@ describe("ListModular on initial rendering", () => {
         render(<ListModular />);
     });
 
+    test("should display 'John's List' ", () => {
+        expect(screen.queryByText(/John's List/)).toBeVisible();
+    });
+
+    test("should display 'Dom's List' ", () => {
+        expect(screen.queryByText(/Dom's List/)).toBeVisible();
+    });
+
     test("should display spotlight graphic", () => {
         expect(screen.queryByAltText(/A pair of hanging spotlights shining on the main image area in the center of the screen/)).toBeVisible();
     });
 
-    test("should display 'null-image' for FeaturedMatch on initial rendering", () => {
+    test("should display a total of 10 list items (5 for each of 2 lists)", () => {
+        const listItems = document.getElementsByClassName("list-item").length;
+        expect(listItems == 10).toBeTruthy();
+    });
+
+    test("should display 'null-image' for FeaturedMatch initially", () => {
         expect(document.getElementById("null-image")).toBeVisible();
     });
 
-    test("should NOT display 'featured-image' on initial rendering", () => {
+    test("should NOT display 'featured-image' initially", () => {
         expect(document.getElementById("featured-image")).toBeNull();
-    });
-
-    
-    
+    });   
 });
