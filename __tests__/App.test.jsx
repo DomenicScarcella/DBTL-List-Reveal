a902ad45bd06e38f1de2ab847ea618fc31a4f618
unknown <ls41513@yahoo.com>
2023-04-22 19:03:39 -0400

    Detail 'Arrange, Act, Assert' as comments in test files

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -5,69 +5,87 @@ import { App } from "../src/App.jsx";
 
 describe("App on initial rendering", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<App />);
     });
 
     test("should display '#DontBuryTheLead' ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/#DontBuryTheLead/)).toBeVisible();
     });
 
     test("should display 'PREVIEW:' ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/PREVIEW:/)).toBeVisible();
     });
 
     test("should display 'Top 5 Matches We Want To See' ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/Top 5 Matches We Want To See/)).toBeVisible();
     });
 
     test("should display large wmlogo", () => {
+        // ACT, ASSERT 
         expect(screen.queryByAltText(/Large graphic of WrestleMania 39 logo/)).toBeVisible();
     });
 
     test("should NOT display 'John's List' initially ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/John's List/)).toBeNull();
     });
 
     test("should NOT display 'Dom's List' initially ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/Dom's List/)).toBeNull();
     });
 
     test("should NOT display spotlight graphic initially", () => {
+        // ACT, ASSERT 
         expect(screen.queryByAltText(/A pair of hanging spotlights shining on the main image area in the center of the screen/)).toBeNull();
     });
 });
 
 describe("App after clicking wmlogo img (id='open-logo') ", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<App />);
+
+        // ACT 
         fireEvent.click(document.getElementById("open-logo"));
     });
 
     test("should still display '#DontBuryTheLead' ", () => {
+        // ASSERT 
         expect(screen.queryByText(/#DontBuryTheLead/)).toBeVisible();
     });
 
     test("should still display 'PREVIEW:' ", () => {
+        // ASSERT 
         expect(screen.queryByText(/PREVIEW:/)).toBeVisible();
     });
 
     test("should still display 'Top 5 Matches We Want To See' ", () => {
+        // ASSERT 
         expect(screen.queryByText(/Top 5 Matches We Want To See/)).toBeVisible();
     });
 
     test("should NOT display large wmlogo after click on wmlogo", () => {
+        // ASSERT 
         expect(screen.queryByAltText(/Large graphic of WrestleMania 39 logo/)).toBeNull();
     });
 
     test("should display 'John's List' after click on wmlogo", () => {
+        // ASSERT 
         expect(screen.queryByText(/John's List/)).toBeVisible();
     });
 
     test("should display 'Dom's List' after click on wmlogo", () => {
+        // ASSERT 
         expect(screen.queryByText(/Dom's List/)).toBeVisible();
     });
 
     test("should display spotlight graphic after click on wmlogo", () => {
+        // ASSERT 
         expect(screen.queryByAltText(/A pair of hanging spotlights shining on the main image area in the center of the screen/)).toBeVisible();
     });
 });
--- __tests__/ListModular.test.jsx
+++ __tests__/ListModular.test.jsx
@@ -5,80 +5,121 @@ import { ListModular } from "../src/ListModular.jsx";
 
 describe("ListModular on initial rendering", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<ListModular />);
     });
 
     test("should display 'John's List' ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/John's List/)).toBeVisible();
     });
 
     test("should display 'Dom's List' ", () => {
+        // ACT, ASSERT 
         expect(screen.queryByText(/Dom's List/)).toBeVisible();
     });
 
     test("should display spotlight graphic", () => {
+        // ACT, ASSERT 
         expect(screen.queryByAltText(/A pair of hanging spotlights shining on the main image area in the center of the screen/)).toBeVisible();
     });
 
     test("should display a total of 10 list items (5 for each of 2 lists)", () => {
+        // ACT 
         const listItems = document.getElementsByClassName("list-item").length;
+
+        // ASSERT 
         expect(listItems == 10).toBeTruthy();
     });
 
     test("all 10 list items should also have 'hide' class", () => {
+        // ACT 
         const listItems = document.getElementsByClassName("list-item hide").length;
+        
+        // ASSERT 
         expect(listItems == 10).toBeTruthy();
     });
 
     test("zero list items should have 'unhide' class", () => {
+        // ACT 
         const listItems = document.getElementsByClassName("list-item unhide").length;
+        
+        // ASSERT 
         expect(listItems == 0).toBeTruthy();
     });
 
     test("title text for Rhea Ripley matches should technically render -- twice -- even though styling hides visibility", () => {
+        // ACT 
         const rheas = screen.queryAllByText(/ vs. Rhea Ripley/).length;
+        
+        // ASSERT 
         expect(rheas == 2).toBeTruthy();
     });
 
     test("should display 'null-image' for FeaturedMatch initially", () => {
+        // ACT, ASSERT
         expect(document.getElementById("null-image")).toBeVisible();
     });
 
     test("should NOT display 'featured-image' initially", () => {
+        // ACT, ASSERT 
         expect(document.getElementById("featured-image")).toBeNull();
     });   
 });
 
 describe("ListModular changes when clicking same list item once, then twice", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<ListModular />);
     });
 
     test("should unhide list item L1 after 1st click", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
+
+        // ASSERT 
         expect(document.querySelector("button.list-item.L.hide.L1")).toBeNull();
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
     });
 
     test("should set list item to id=active-match-L after 1 click", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
+        
+        // ASSERT 
         expect(document.querySelector("button#active-match-L")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
     });
 
     test("should display image for FeaturedMatch after 1 click", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
+        
+        // ASSERT 
         expect(document.querySelector("img#featured-image")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
     });
 
     test("should re-hide list item L1 AND undo id=active-match-L AND remove FeaturedMatch image after 2nd click on same list item", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
+        
+        // ASSERT 
         expect(document.querySelector("button.list-item.L.hide.L1")).toBeNull();
         expect(document.querySelector("button.list-item.L.unhide.L1#active-match-L")).toBeVisible();
         expect(document.querySelector("img#featured-image")).toBeVisible();
+
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
+        
+        // ASSERT 
         expect(document.querySelector("img#null-image")).toBeVisible();
         expect(document.querySelector("button#active-match-L")).toBeNull();
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeNull();
@@ -88,21 +129,27 @@ describe("ListModular changes when clicking same list item once, then twice", ()
 
 describe("ListModular clicking several list items", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<ListModular />);
     });
 
     test("clicking 5 list items once should unhide each of them, and set FeaturedMatch info to the last of those five clicks", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L3"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L4"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L5"));
+        
+        // ASSERT 
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L2")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L3")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L4")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L5#active-match-L")).toBeVisible();
         expect(document.querySelector("img#featured-image")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L3"));
@@ -111,18 +158,23 @@ describe("ListModular clicking several list items", () => {
     });
 
     test("clicking all 5 listL items once should unhide each of them, and clicking 1 of the newly unhide listL items should hide it while leaving the other 4 items in unhide", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L3"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L4"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L5"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L3"));
+        
+        // ASSERT 
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L2")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L3")).toBeNull();
         expect(document.querySelector("button.list-item.L.unhide.L4")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L5#active-match-L")).toBeVisible();
         expect(document.querySelector("img#featured-image")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L4"));
@@ -130,12 +182,15 @@ describe("ListModular clicking several list items", () => {
     });
 
     test("when all of listL is unhide, clicking list-item R1 should activate R1 and the similar list-item in listL (L4)", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L3"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L4"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L5"));
         fireEvent.click(document.querySelector("button.list-item.R.hide.R1"));
+        
+        // ASSERT 
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L2")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L3")).toBeVisible();
@@ -143,6 +198,8 @@ describe("ListModular clicking several list items", () => {
         expect(document.querySelector("button.list-item.L.unhide.L5")).toBeVisible();
         expect(document.querySelector("button.list-item.R.unhide.R1#active-match-R")).toBeVisible();
         expect(document.querySelector("img#featured-image")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L3"));
@@ -154,10 +211,12 @@ describe("ListModular clicking several list items", () => {
 
 describe("ListModular clicking the featured-image when FeaturedMatch has active image displayed", () => {
     beforeEach(() => {
+        // ARRANGE 
         render(<ListModular />);
     });
 
     test("unhide all list items and click on featured-image should remove featured-image but leave all list items in unhide", () => {
+        // ACT 
         fireEvent.click(document.querySelector("button.list-item.L.hide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.hide.L3"));
@@ -168,8 +227,14 @@ describe("ListModular clicking the featured-image when FeaturedMatch has active 
         fireEvent.click(document.querySelector("button.list-item.R.hide.R3"));
         fireEvent.click(document.querySelector("button.list-item.R.hide.R4"));
         fireEvent.click(document.querySelector("button.list-item.R.hide.R5"));
+        
+        // ASSERT 
         expect(document.querySelector("img#featured-image")).toBeVisible();
+        
+        // ACT 
         fireEvent.click(document.querySelector("img#featured-image"));
+        
+        // ASSERT 
         expect(document.querySelector("img#featured-image")).toBeNull();
         expect(document.querySelector("button.list-item.L.unhide.L1")).toBeVisible();
         expect(document.querySelector("button.list-item.L.unhide.L2")).toBeVisible();
@@ -181,6 +246,8 @@ describe("ListModular clicking the featured-image when FeaturedMatch has active 
         expect(document.querySelector("button.list-item.R.unhide.R3")).toBeVisible();
         expect(document.querySelector("button.list-item.R.unhide.R4")).toBeVisible();
         expect(document.querySelector("button.list-item.R.unhide.R5")).toBeVisible();
+        
+        // RETURN TO PRE-TEST STATE 
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L1"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L2"));
         fireEvent.click(document.querySelector("button.list-item.L.unhide.L3"));
@@ -191,9 +258,5 @@ describe("ListModular clicking the featured-image when FeaturedMatch has active 
         fireEvent.click(document.querySelector("button.list-item.R.unhide.R3"));
         fireEvent.click(document.querySelector("button.list-item.R.unhide.R4"));
         fireEvent.click(document.querySelector("button.list-item.R.unhide.R5"));
-
     });
 });
-
-
-
