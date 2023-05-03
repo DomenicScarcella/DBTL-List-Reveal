743b844da9d6682a326b9776596e613d90d3a309
unknown <ls41513@yahoo.com>
2023-05-03 01:55:42 -0400

    Add base: '/DBTL-List-Reveal/' to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './__tests__/setup.js',
-      css: true,
-      coverage: {
-         reporter: ['text', 'json', 'html'],
+   base: '/DBTL-List-Reveal/',
+   plugins: [react()],
+      test: {
+         globals: true,
+         environment: 'jsdom',
+         setupFiles: './__tests__/setup.js',
+         css: true,
+         coverage: {
+            reporter: ['text', 'json', 'html'],
+         },
       },
-   },
 });
